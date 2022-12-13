d664c9680053f9c806b0b057e055a17207eb8b3a
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-13 08:28:35 -0800

    index

---
+++ main.js
@@ -0,0 +1,28 @@
+
+/*let num = parseInt (prompt("Enter the value"));
+
+let sum = 0;
+
+for (var i = 0; i < num; ++i) {
+    if (num> 0) {
+        
+    }
+}*/
+
+let a=parseInt (prompt("Enter the value"));
+
+for (var x=0; x<=a; x++) {
+    if (x >0) {
+            console.log(x +  " is even");
+    }
+
+    else {
+        console.log(x +  " negative");
+    }
+
+    }
+  
+
+    
+
+
