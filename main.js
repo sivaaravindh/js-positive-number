91206f0eb79f7fbee85c876330f23d90c6c93f17
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-13 20:54:07 -0800

    index

--- main.js
+++ main.js
@@ -9,20 +9,58 @@ for (var i = 0; i < num; ++i) {
     }
 }*/
 
-let a=parseInt (prompt("Enter the value"));
+/*let a;
+let add=0;
+for (let x=0; x<=a; x++) {
 
-for (var x=0; x<=a; x++) {
+    a=parseInt (prompt("Enter the value"));
+    for (let j=1; j<=a; j++){
+
     if (x >0) {
             console.log(x +  " is even");
     }
-
-    else {
-        console.log(x +  " negative");
+    
+    add += x;
     }
+    document.write("The sum of positive numbers" + add)
+}
+var numbers = [1, 2, 3, -4, -5, -6];
 
+var sum = 0;
+
+for (var i = 0; i < numbers.length; ++i) {
+    if (numbers[i] > 0) {
+        sum += numbers[i];
     }
+}
+
+console.log(sum);
+
+const posnum=parseInt (prompt("Enter the value"));
+    let sum = 0;
+    for (let i = 0; i < posnum; ++i) {
+        if (i > 0) {
+            sum += posnum;
+        }
+    }
+    console.log(sum);*/
+
+    let num=parseInt (prompt("Enter the value"));
+    let sum= 0;
+    for(let i=1; i<=num; i++){
+            let b=parseInt (prompt("Enter the value"));
+
+            if(b>0){
+                sum +=b;
+            }
+    }
+    
+    document.write(sum);
+
+
+
   
-
+   
     
 
 
